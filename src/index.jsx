79f0c69c55e5a4c04fd4e5040e334e9419201767
AddNiI <kola.kolya876@gmail.com>
2026-02-23 17:55:03 +0200

    fix 9

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import Login from './site/login.jsx';
 import Library from './site/library.jsx'
 import Registration from './site/register.jsx';
@@ -11,7 +10,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root')).render(
   <PageStateProvider>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
             <Route path="*" element={<Navigate to="/login" replace />} />
             <Route path="/login" element={<Login />} />
