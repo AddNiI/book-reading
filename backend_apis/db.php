2febb8014431811d985743f96776940156d34299
AddNiI <100078200+AddNiI@users.noreply.github.com>
2026-03-04 13:45:30 +0200

    04.03.2026
    safety fix

--- backend_apis/db.php
+++ backend_apis/db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql308.infinityfree.com";
-$username = "if0_41257518";
-$password = "ya1ne2znayu";
-$dbname = "if0_41257518_users";
+$servername = "****";
+$username = "****";
+$password = "****";
+$dbname = "****";
 $mysqli = new mysqli($servername, $username, $password, $dbname);
 mysqli_set_charset($mysqli, "utf8mb4");
 if ($mysqli->connect_error) {
